a26210f26d60059f28c34d0fc86aa4d38953fefa
ButtKeC0de <carlos_buttke@estudante.sesisenai.org.br>
2026-03-17 19:34:51 -0300

    ADD-EXEMPLO

---
+++ script.js
@@ -0,0 +1,23 @@
+    function ex1(){
+     let soma = 0;
+     let continuar = true;
+
+     while(continuar){
+        let numero = parseFloat(prompt("Digite um número (Ou zero para encerrar): "));
+
+        //modelo antigo:soma = soma + numero;
+        //modelo java:soma += numero;
+
+        if(isNaN(numero)){
+            alert("Por favor, digite um número válido!");
+
+        }else{
+            soma += numero;
+        }
+
+        continuar = confirm("Deseja adicionar mais um número?");
+
+    }
+    alert("A soma dos números é " + soma);
+
+}
