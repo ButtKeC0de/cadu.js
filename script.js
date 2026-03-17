3faeb507ae3e7116fade468d048aed3d30b9dd5a
ButtKeC0de <carlos_buttke@estudante.sesisenai.org.br>
2026-03-17 19:38:51 -0300

    ADD - EXERCICIO 2

--- script.js
+++ script.js
@@ -36,3 +36,52 @@ function exercicio_1(){
 }
 }
 
+function exercicio_2(){
+    
+    function mensal(){
+    
+    let gn_total = 0;
+    let pd_total = 0;
+    let mess = 1;
+    let sld_total = 0;
+
+    while(mess<=12){
+        let bruto = parseFloat(prompt("Digite qual foi o ganho bruto do mês " + mess ));
+
+        if(isNaN(bruto)){
+            alert("Por favor, digite um número válido!");
+
+        }else{
+           gn_total+=bruto;
+        }
+        
+        let gasto = parseFloat(prompt("Digite qual foi o gasto do mês " + mess ));
+
+        if(isNaN(gasto)){
+            alert("Por favor, digite um número válido!");
+
+        }else{
+           pd_total+=gasto;
+        }
+        
+        mess++;
+    } 
+        alert("Seu bruto anual foi de : " + gn_total);
+        alert("Seu gasto anual foi de: " + pd_total); 
+
+        sld_total= gn_total-pd_total;
+
+        alert("Seu saldo financeiro foi de: " + sld_total);
+         
+        if(sld_total>0){
+            alert("Você lucrou esse ano!")
+
+        } else{
+            alert("Você teve prejuízo esse ano!")
+        }
+    }
+ 
+      calculo_mensal();
+
+}
+
